2746698d0aae00f0b31996ffd47563b6b2bd451c
Yonatan Skourjinsky <yoni.sku@gmail.com>
2025-10-11 21:25:58 +0300

    Add gh-pages deployment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/trucks/", // For github pages
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
